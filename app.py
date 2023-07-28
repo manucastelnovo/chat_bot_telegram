4e2c3f12212476531c46abb19296a4658f7bd31a
jazgamarra <j44min@gmail.com>
2023-07-28 15:26:12 -0400

    refactorizado

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from service.telegram_service import print_message,init_telegram_listener
+from service.telegram_service import TelegramService
 
 TOKEN = '6431750259:AAFkqdNr4w4e_gsPlpDiS1FJq8-g6y3E6Is'
 
 if __name__ == '__main__': 
-    init_telegram_listener(TOKEN,print_message)
+    TelegramService(TOKEN)
--- service/telegram_service.py
+++ service/telegram_service.py
@@ -1,16 +1,20 @@
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters 
 
+class TelegramService(): 
 
+    def __init__(self, env_token): 
+        self.message = None 
+        self.my_bot = telegram.Bot(token =  env_token)
+        self.create_listener
        
-def print_message (update, context):   
-    text = update.message.text
-    print(text)
+    def create_listener(self): 
+        updater = Updater(self.my_bot.token, use_context=True)
+        dp = updater.dispatcher  
+        dp.add_handler(MessageHandler (Filters.text, self.print_message))
+        updater.start_polling()
        
-
-def init_telegram_listener(env_token,call_back):
-    myBot = telegram.Bot(token =  env_token)
-    updater = Updater(myBot.token, use_context=True)
-    dp = updater.dispatcher  
-    dp.add_handler(MessageHandler (Filters.text, call_back))
-    updater.start_polling()
+    def print_message (update, context, self):   
+        self.message = update.message.text
+        
+   
